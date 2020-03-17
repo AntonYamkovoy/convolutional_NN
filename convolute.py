974682eb43e18a0b897e97742ccaf2d2144c96c7
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2020-03-17 13:05:44 +0000

    added skeleton of some function defs

--- convolute.py
+++ convolute.py
@@ -7,6 +7,87 @@
 import cv2
 import numpy as np
 
+
+
+# description
+# needs to generate random image sizes within a range, and kernels
+
+# params
+# @ max height - maximim height of images
+# @ max width - max width of images
+# @ max kernel size
+
+# @ outputs, structure to store images and kernels sets.
+
+def create_image_set():
+
+    return
+
+
+
+
+# description
+# calculates the average results for the given random generated (image,kernel) set
+
+# params
+# @ image set
+# @ kernel set
+
+# @ outputs the resulting approximation matrix, to be compared with toom cook algo outputs
+
+def calc_average():
+
+    return
+
+
+
+
+# description
+# generates toom cook algo for fast convolution
+
+# params
+# @ points set for toom cook
+# @ image set
+# @ kernel set
+
+# @ outputs a result using the generatd toom cook algo based on the points inputted
+
+def generate_toom_cook_results():
+
+    return
+
+
+# description
+# compares toom cook generated matrix to direct convolution matrix
+
+# params
+# @ toom cook results matrix
+# @ direct results matrix
+
+
+# @ error rate, in comparison to base
+
+def compare_results():
+
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # load the image and scale to 0..1
 image_input = cv2.imread('test.jpg', cv2.IMREAD_GRAYSCALE).astype(float) / 255.0
 
