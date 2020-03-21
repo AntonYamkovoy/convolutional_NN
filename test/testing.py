a758c5d1bdc38bc9266a8316d20aa497e7aea39b
AntonYamkovoy <42891596+AntonYamkovoy@users.noreply.github.com>
2020-03-21 19:32:32 +0000

    implemented a function for testing points of various configurations

--- test/testing.py
+++ test/testing.py
@@ -162,6 +162,183 @@ def showCookToomConvolution(a,n,r,fractionsIn=FractionsInG):
         print("")
 
 
+
+
+
+
+# input size = a = m + r -1
+# kernel size = r
+# output size = r
+# number of points = m + r - 2
+
+# function for testing 2d convolution on a variable amount of points
+def test_points_variable(points, image, kernel ,image_size, kernel_size):
+
+    points_tuple = tuple(points)
+    if len(points) > ((image_size + kernel_size ) - 2):
+        return None
+    # points list is of the correct size
+    A,G,B,f=  cookToomFilter(points_tuple, image_size, kernel_size)
+
+    tempA = np.asarray(A)
+    tempB = np.asarray(B)
+    finalG = np.asarray(G, dtype=np.int32)
+    finalA = np.asarray(np.transpose(tempA), dtype=np.int32)
+    finalB = np.asarray(np.transpose(tempB), dtype=np.int32)
+    g = image
+    f = kernel
+    direct = np.zeros((image_size,image_size))
+    for i in range(image_size):
+        for j in range(image_size):
+            direct[i,j] = np.sum(f * g[i:i+kernel_size,j:j+kernel_size])
+
+    inner = np.dot(finalG, np.dot(f, finalG.T)) * np.dot(finalB.T, np.dot(g, finalB))
+    Y = np.dot(finalA.T, np.dot(inner, finalA))
+
+    print("Error of one Winograd",la.norm(Y - direct)/la.norm(direct))
+
+    convLib = scisig.convolve2d(f,g)
+    conv2d = convolve2DToeplitz(f,g)
+    g2 = revMatrix(g)
+    if image_size == 4:
+        g2 = padImage(g2,len(f)-1)
+    else:
+        g2 = padImage(g2,len(f))
+
+
+    #print("g2: ",g2.shape)
+    cWino = simpleWinogradAlg(f,g2,image_size,finalB,finalG,finalA)[0]
+    cWino = revMatrix(cWino)
+    cWino = padImage(cWino,len(f))
+
+    print("Standard Error:",la.norm( convLib - conv2d)/la.norm(convLib))
+    #final_error = la.norm(convLib - cWino)/la.norm(convLib)
+    final_error =la.norm( convLib- cWino) / la.norm(convLib)
+    #final_error =0
+    print("Winograd/TC Error:",final_error)
+
+
+    return final_error
+
+
+
+"""
+final implementation plan
+
+1. generate set of (images,kernels) that will be used to test the various sets of points.
+    - will have to be uniform size for image, kernel and randomized
+
+2. for each image set, corresponding toom cook algorithms will be built based on the set
+   trying different sets of points to minimize Error
+
+3. for each image,kernel set, there will be a separate structure to store
+   the best points ranked that we have tested
+
+4. we also possibly can implement 1d and 2d convolution versions for different sets
+
+"""
+""" returns a list of random (images,kernels) for given dimensions, of length size"""
+def generate_set(image_size,kernel_size, size):
+    list= []
+    for i in range(size):
+        image = np.random.random((image_size,image_size))
+        kernel = np.random.random((kernel_size,kernel_size))
+        list.append((image,kernel))
+
+    return list
+
+
+
+
+# input size = a = m + r -1
+# kernel size = r
+# output size = r
+# number of points = m + r - 2
+
+# function for testing 2d convolution on a variable amount of points
+def test_points23(points, image, kernel ,image_size, kernel_size):
+
+    points_tuple = tuple(points)
+    if len(points) > ((image_size + kernel_size ) - 2):
+        return None
+    # points list is of the correct size
+    A,G,B,f2=  cookToomFilter(points_tuple, image_size, kernel_size)
+
+    tempA = np.asarray(A)
+    tempB = np.asarray(B)
+    finalG = np.asarray(G, dtype=np.int32)
+    finalA = np.asarray(np.transpose(tempA), dtype=np.int32)
+    finalB = np.asarray(np.transpose(tempB), dtype=np.int32)
+    g = image
+    f = kernel
+    direct = np.zeros((2,2))
+    for i in range(2):
+        for j in range(2):
+            direct[i,j] = np.sum(f * g[i:i+3,j:j+3])
+
+    inner = np.dot(finalG, np.dot(f, finalG.T)) * np.dot(finalB.T, np.dot(g, finalB))
+    Y = np.dot(finalA.T, np.dot(inner, finalA))
+
+    #print("Error of one Winograd",la.norm(Y - direct)/la.norm(direct))
+
+    convLib = scisig.convolve2d(f,g)
+    conv2d = convolve2DToeplitz(f,g)
+    g2 = revMatrix(g)
+    g2 = padImage(g2,len(f))
+    cWino = simpleWinogradAlg(f,g2,2,finalB,finalG,finalA)[0]
+    cWino = revMatrix(cWino)
+
+    #print("Standard Error:",la.norm(convLib - conv2d, ord=2)/la.norm(convLib, ord=2))
+    final_error = la.norm(convLib - cWino, ord=2)/la.norm(convLib, ord=2)
+    print("Winograd/TC Error:",final_error)
+
+
+    return final_error
+
+
+print("ex 23- 1")
+image_size = 2
+kernel_size = 3
+points = [0.5,0.2,-0.1]
+image = np.random.random((image_size+kernel_size-1,image_size+kernel_size-1))
+kernel =  np.random.random((kernel_size,kernel_size))
+
+error = test_points23(points,image,kernel,image_size,kernel_size)
+
+print("ex 23- 2")
+image_size = 2
+kernel_size = 3
+points = [0,-1,2]
+image = np.random.random((image_size+kernel_size-1,image_size+kernel_size-1))
+kernel =  np.random.random((kernel_size,kernel_size))
+
+error = test_points23(points,image,kernel,image_size,kernel_size)
+
+
+print("ex 23- 2")
+image_size = 6
+kernel_size = 3
+points = [-1.5,-1,-0.5,0,0.5,1,1.5] # number of points = (6+3)-2 = 7
+image = np.random.random((image_size+kernel_size-1,image_size+kernel_size-1))
+kernel =  np.random.random((kernel_size,kernel_size))
+
+error = test_points_variable(points,image,kernel,image_size,kernel_size)
+
+
+
+print("ex 23- 2")
+image_size = 4
+kernel_size = 3
+points = [-1,-0.5,0,0.5,1] # number of points = (4+3)-2 = 5
+image = np.random.random((image_size+kernel_size-1,image_size+kernel_size-1))
+kernel =  np.random.random((kernel_size,kernel_size))
+
+error = test_points_variable(points,image,kernel,image_size,kernel_size)
+
+
+
+
+
 """
 B23 = np.asarray([
     [1, 0,-1, 0],
@@ -186,7 +363,7 @@ A23 = np.asarray([
 
 """
 
-
+"""
 # need to generate A23, G23, B23 matrices
 #------------------------------------------
 A,G,B,f=  cookToomFilter((0,1,-1), 2, 3)
@@ -237,3 +414,4 @@ cWino = revMatrix(cWino)
 
 print("Standard Error:",la.norm(convLib - conv2d, ord=2)/la.norm(convLib, ord=2))
 print("Winograd/TC Error:",la.norm(convLib - cWino, ord=2)/la.norm(convLib, ord=2))
+"""
